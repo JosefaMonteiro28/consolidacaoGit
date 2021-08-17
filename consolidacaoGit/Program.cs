1519053db47e4b5e23345480c25e2c6527caf813
unknown <josefajosemonteiro28@gmail.com>
2021-08-17 15:17:51 +0100

    initial commit

---
+++ consolidacaoGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consolidacaoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
