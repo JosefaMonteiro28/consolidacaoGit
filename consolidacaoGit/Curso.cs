f0a4cc9330123b35bef4c9d9f45ed7e29d0fff04
unknown <josefajosemonteiro28@gmail.com>
2021-08-17 15:28:19 +0100

    adicaoa da classe curso

---
+++ consolidacaoGit/Curso.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolidacaoGit
+{
+    public class Curso
+    {
+        public int IdCurso { get; set; }
+        public string Nome { get; set; }
+        public ICollection<Aluno> Alunos { get; set; }
+    }
+}
